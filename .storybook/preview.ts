9c468a0e9361774d6dbe0fd905dbab3c79c1522e
JulianVIE <julian.hiess@gmail.com>
2026-04-23 15:40:03 +0200

    #288 storybook change

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,7 +2,7 @@ import type { Preview } from "@storybook/react-vite";
 import "../src/index.css";
 import { createElement, Fragment, type ReactElement, useEffect } from "react";
 
-import { Toaster } from "../src/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 
 type ThemeMode = "light" | "dark";
 
